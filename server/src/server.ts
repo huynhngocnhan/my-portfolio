5a690256a8d9dda16c46d2714d3affbf41179627
huynhngocnhan <huynhngocnhan@gmail.com>
2025-10-19 09:18:47 +0700

    add base code project

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI as string;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
